ae861584b1d550e5091a2e74dfbcb1e033be1f00
xym760 <1455422076@qq.com>
2019-06-14 18:11:34 +0800

    spring mvc

--- src/com/nxist/springmvc/handlers/SpringMvcTest.java
+++ src/com/nxist/springmvc/handlers/SpringMvcTest.java
@@ -2,6 +2,7 @@ package com.nxist.springmvc.handlers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @Author: xym760
@@ -12,6 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/springmvc")
 public class SpringMvcTest {
     private static final String SUCCESS = "success";
+
+    /**
+     * 使用method属性来指定请求方式
+     * @return
+     */
+    @RequestMapping(value = "/testMethod",method = RequestMethod.POST)
+    public String testMethod(){
+        System.out.println("testMethod");
+        return SUCCESS;
+    }
 
     @RequestMapping("/testRequestMapping")
     public String testRequestMapping() {
--- web/index.jsp
+++ web/index.jsp
@@ -7,12 +7,18 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
+<head>
     <title>主页</title>
-  </head>
-  <body>
-  <a href="springmvc/testRequestMapping">Test RequestMapping</a>
-  <br>
+</head>
+<body>
+<form action="springmvc/testMethod" method="post">
+    <input type="submit" value="submit">
+</form>
+<br><br>
+<a href="springmvc/testMethod">Test Method</a>
+<br><br>
+<a href="springmvc/testRequestMapping">Test RequestMapping</a>
+<br><br>
 <a href="helloworld">hello world</a>
-  </body>
+</body>
 </html>
